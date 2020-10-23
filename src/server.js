79e8e9c909f1714601e0a349fc9e645387a33439
TheMartonfi <carlmartonfi@hotmail.com>
2020-10-23 17:42:45 -0600

    Establish connection to db

--- src/server.js
+++ src/server.js
@@ -3,18 +3,21 @@ require("dotenv").config();
 const PORT = process.env.PORT || 8080;
 const ENV = process.env.ENV || "development";
 const express = require("express");
-// const cookieSession = require("cookie-session");
+const { Pool } = require("pg");
 const morgan = require("morgan");
 
 const app = express();
 app.use(morgan("dev"));
 
-// const { Pool } = require('pg');
-// const dbParams = require('./lib/db.js');
-// const db = new Pool(dbParams);
-// db.connect();
+const dbParams = { connectionString: process.env.DATABASE_URL || "" };
+const db = new Pool(dbParams);
+db.connect();
 
-// const usersRoutes = require("./routes/users");
+// db.query
+
+// app.use("/api", days(db));
+// app.use("/api", appointments(db, actions.updateAppointment));
+// app.use("/api", interviewers(db));
 
 app.get("/", (req, res) => {
 	res.json({ data: "type" });
