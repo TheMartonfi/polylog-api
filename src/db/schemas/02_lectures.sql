02c6336b98b5ee34eb27773a1c436491a9cb99bb
TheMartonfi <carlmartonfi@hotmail.com>
2020-10-29 20:51:41 -0600

    Remove NOT NULL constraint from lectures table

--- src/db/schemas/02_lectures.sql
+++ src/db/schemas/02_lectures.sql
@@ -3,6 +3,6 @@ DROP TABLE IF EXISTS lectures CASCADE;
 CREATE TABLE lectures (
   id SERIAL PRIMARY KEY NOT NULL,
   lecturer_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  title VARCHAR(255) NOT NULL,
+  title VARCHAR(255),
   description TEXT DEFAULT 'No description'
 );
