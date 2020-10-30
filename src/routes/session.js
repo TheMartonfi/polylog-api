061e3ddd0bd89e778e0b83de58195389095c4247
TheMartonfi <carlmartonfi@hotmail.com>
2020-10-30 01:43:02 -0600

    Make sql query comments for getting lecture session history data

--- src/routes/session.js
+++ src/routes/session.js
@@ -16,6 +16,74 @@ module.exports = db => {
 		).then(({ rows: sessions }) => res.json(sessions));
 	});
 
+	// SELECT sessions.created_at FROM sessions
+	// WHERE sessions.lecture_id = 1
+	// ORDER BY sessions.created_at;
+
+	// SELECT COUNT(*) FROM sessions
+	// WHERE sessions.lecture_id = 1;
+
+	// SELECT COUNT(*) FROM attendees
+	// JOIN sessions ON sessions.id = attendees.session_id
+	// JOIN lectures ON lectures.id = sessions.lecture_id;
+
+	// SELECT AVG(attendees.count) AS attendees_avg
+	// FROM (
+	// SELECT COUNT(*)
+	// FROM attendees
+	// JOIN sessions ON sessions.id = attendees.session_id
+	// JOIN lectures ON lectures.id = sessions.lecture_id
+	// WHERE lectures.id = 1
+	// GROUP BY attendees.session_id
+	// 	) AS attendees;
+
+	// SELECT COUNT(*) FROM topic_reactions
+	// JOIN topic_cards
+	// ON topic_cards.id = topic_reactions.topic_card_id
+	// WHERE topic_reactions.reaction = true
+	// AND topic_cards.lecture_id = 1;
+
+	// SELECT COUNT(*) FROM topic_reactions
+	// JOIN topic_cards
+	// ON topic_cards.id = topic_reactions.topic_card_id
+	// WHERE topic_reactions.reaction = false
+	// AND topic_cards.lecture_id = 1;
+
+	// SELECT COUNT(*) FROM topic_responses
+	// JOIN topic_cards
+	// ON topic_cards.id = topic_responses.topic_card_id
+	// WHERE topic_responses.type = 'question'
+	// AND topic_cards.lecture_id = 1;
+
+	// SELECT COUNT(*) FROM topic_responses
+	// JOIN topic_cards
+	// ON topic_cards.id = topic_responses.topic_card_id
+	// WHERE topic_responses.type = 'answer'
+	// AND topic_cards.lecture_id = 2;
+
+	// SELECT COUNT(*) FROM topic_responses
+	// JOIN topic_cards
+	// ON topic_cards.id = topic_responses.topic_card_id
+	// WHERE topic_responses.type = 'comment'
+	// AND topic_cards.lecture_id = 2;
+
+	// 	SELECT COUNT(*) FROM quiz_cards
+	// WHERE quiz_cards.lecture_id = 1;
+
+	// SELECT DISTINCT quiz_responses.id, quiz_answers.correct FROM quiz_responses
+	// JOIN quiz_answers
+	// ON quiz_answers.id = quiz_responses.quiz_answer_id
+	// JOIN quiz_cards ON quiz_cards.id = quiz_responses.quiz_card_id
+	// WHERE quiz_cards.lecture_id = 1
+	// AND quiz_answers.correct = true;
+
+	// SELECT DISTINCT quiz_responses.id, quiz_answers.correct FROM quiz_responses
+	// JOIN quiz_answers
+	// ON quiz_answers.id = quiz_responses.quiz_answer_id
+	// JOIN quiz_cards ON quiz_cards.id = quiz_responses.quiz_card_id
+	// WHERE quiz_cards.lecture_id = 1
+	// AND quiz_answers.correct = false;
+
 	router.post("/", (req, res) => {
 		db.query(
 			`
