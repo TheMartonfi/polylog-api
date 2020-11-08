554429cc7c8b1bd873461acc8a412fb72089a933
TheMartonfi <carlmartonfi@hotmail.com>
2020-11-07 18:02:16 -0700

    Add uuid-ossp extension for sessions table

--- src/db/schemas/03_sessions.sql
+++ src/db/schemas/03_sessions.sql
@@ -1,7 +1,8 @@
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 DROP TABLE IF EXISTS sessions CASCADE;
 
 CREATE TABLE sessions (
-  id uuid DEFAULT uuid_generate_v4 (),
+  id uuid DEFAULT uuid_generate_v4(),
   lecture_id INTEGER REFERENCES lectures(id) ON DELETE CASCADE,
   created_at TIMESTAMP NOT NULL,
   start_time TIME NOT NULL,
